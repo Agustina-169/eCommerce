eb021439483e80932df3d769c752656491ecef28
Agustina-169 <ailenagustina169@gmail.com>
2022-09-06 20:50:43 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Product from './Components/Product';
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hola</h1>
+      <Navbar/>
       <Product/>
     </div>
     
