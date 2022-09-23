c8cbe9aae7332f953a3588634689aeac6c274855
Agustina-169 <ailenagustina169@gmail.com>
2022-09-23 11:37:05 -0300

    form

--- src/Components/CheckoutForm/Checkout.js
+++ src/Components/CheckoutForm/Checkout.js
@@ -1,8 +1,17 @@
-import { Paper, Typography } from '@material-ui/core';
+import { Paper, Step, StepLabel, Stepper, Typography } from '@material-ui/core';
 import useStyles from './styles';
+import { useState } from 'react';
+import AddressForm from './AddressForm';
+import PaymentForm from './PaymentForm';
 
 export const Checkout = () => {
   const classes = useStyles();
+  const [activeStep, setActiveStep] = useState(0);
+  const steps = ['Shipping address', 'Payment details'/*, 'Review your order'*/];
+  const nextStep = () => setActiveStep((prevActiveStep)=> prevActiveStep +1);
+  const backStep = () => setActiveStep((prevActiveStep)=> prevActiveStep -1);
+
+  const Form = () => activeStep === 0 ? <AddressForm/> : <PaymentForm/>
 
   return (
     <>
@@ -11,10 +20,21 @@ export const Checkout = () => {
         <Typography component='h1' variant='h4' align='center' > 
         checkout
         </Typography>
+        <Stepper activeStep={activeStep} className={classes.stepper}>
+          {steps.map(step =>(
+            <Step key={step}>
+              <StepLabel>
+                {step}
+              </StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+        <Form/>
       </Paper>
+      
       </main>
     </>
-    
+
   )
 }
 
